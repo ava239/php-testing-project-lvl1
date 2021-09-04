eccab8b9985092e41894f9229ad76c3f9767203c
Ava <ava239@yandex.ru>
2021-09-05 01:34:02 +0300

    first tests and simplest implementation

--- src/Core.php
+++ src/Core.php
@@ -2,26 +2,39 @@
 
 namespace Ava239\Page\Loader;
 
-use Exception;
+use GuzzleHttp\Client;
 
 class Core
 {
     private string $outputDir;
+    private Client $httpClient;
 
-    public function __constructor(string $output = './'): void
+    public function __construct(string $outputDir, Client $httpClient = null)
     {
-        $realDirpath = realpath($output);
-        if ($realDirpath === false || !is_dir($realDirpath)) {
-            throw new Exception("'$output' is not a directory");
-        }
-        if (!is_readable($realDirpath)) {
-            throw new Exception("Can`t read '$output'");
-        }
-        $this->outputDir = $realDirpath;
+        $realDirpath = realpath($outputDir);
+        $this->outputDir = $realDirpath !== false
+            ? $realDirpath
+            : $outputDir;
+        $this->httpClient = $httpClient ?? new Client();
     }
 
     public function download(string $url): string
     {
+        $data = $this->load($url);
+
+        file_put_contents("{$this->outputDir}/{$this->generateFileName($url)}", $data);
+
         return "Page was successfully loaded into {$this->outputDir}";
+    }
+
+    public function load(string $url): string
+    {
+        return $this->httpClient->get($url)->getBody()->getContents();
+    }
+
+    public function generateFileName(string $url): string
+    {
+        $scheme = parse_url($url, PHP_URL_SCHEME);
+        return preg_replace(["~$scheme://~", '~[^\d\w]~'], ['', '-'], $url) . '.html';
     }
 }
--- tests/DownloadTest.php
+++ tests/DownloadTest.php
@@ -2,12 +2,60 @@
 
 namespace Ava239\Page\Loader\Tests;
 
+use Ava239\Page\Loader\Core;
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamDirectory;
+use Faker\Generator;
+use Faker\Factory;
 use PHPUnit\Framework\TestCase;
 
 class DownloadTest extends TestCase
 {
-    public function testOk(): void
+    private MockHandler $mock;
+    private Client $httpClient;
+    private vfsStreamDirectory $root;
+    private Generator $faker;
+    private Core $loader;
+
+    public function getFixtureFullPath(string $fixtureName): string
     {
-        $this->assertTrue(true);
+        $parts = [__DIR__, 'fixtures', $fixtureName];
+        $path = realpath(implode('/', $parts));
+        return $path ?: '';
+    }
+
+    public function setUp(): void
+    {
+        $this->faker = Factory::create();
+        $this->mock = new MockHandler([]);
+        $this->httpClient = new Client(['handler' => HandlerStack::create($this->mock)]);
+        $this->root = vfsStream::setup('home');
+        $this->loader = new Core(vfsStream::url('home'), $this->httpClient);
+    }
+
+    public function testSimpleDownload(): void
+    {
+        $directoryPath = vfsStream::url('home');
+        $url = $this->faker->url();
+        $expectedPath = $this->getFixtureFullPath('simple.html');
+        $expectedData = file_get_contents($expectedPath);
+        $scheme = parse_url($url, PHP_URL_SCHEME);
+        $expectedFilename = preg_replace(["~$scheme://~", '~[^\d\w]~'], ['', '-'], $url) . '.html';
+
+        $mockResponse = new Response(200, [], $expectedData ?: '');
+        $this->mock->append($mockResponse);
+
+        $result = $this->loader->download($url);
+
+        $this->assertEquals("Page was successfully loaded into {$directoryPath}", $result);
+
+        $this->assertTrue($this->root->hasChild("$expectedFilename"));
+
+        $actualData = file_get_contents("{$directoryPath}/$expectedFilename");
+        $this->assertEquals($expectedData, $actualData);
     }
 }
